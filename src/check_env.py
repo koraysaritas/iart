e58dc57079fba1d4450402fde94f64b870c1d84f
koraysaritas <koraysaritas@hotmail.com>
2016-07-19 21:17:33 +0300

    Add structure and dependency check

---
+++ src/check_env.py
@@ -0,0 +1,9 @@
+import sys
+import numpy
+import cv2
+
+print(sys.executable)
+print('')
+print('\n'.join(['numpy: ', numpy.__version__, numpy.__file__]))
+print('')
+print('\n'.join(['cv2: ', cv2.__version__, cv2.__file__]))
